4785ea06010f3d5ef31fa408f33cab497648e677
Chaitanya goel <75871756+chaitanyagoel@users.noreply.github.com>
2021-05-31 18:47:29 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,50 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+    this.rank=null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+
+  getRank(){
+    var playerRankRef = database.ref('rank');
+    playerRankRef.on("value",(data)=>{
+      this.rank = data.val();
+    })
+  }
+
+  static updateRank(count){
+    database.ref('/').update({
+      rank: count
+    });
+  }
+  
+}
